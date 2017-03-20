656195ee353a305b588e9886dcf8a03fd380db49
Katharine <krsibbald@gmail.com>
2017-03-20 16:29:14 -0400

    better defined models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
+
+  validates :post, presence: true
+  validates :user, presence: true
+  validates :body, presence: true
+  validates :email, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :posts
+  has_many :comments
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
