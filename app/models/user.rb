0c749649bf7b4b3f22c005911ae9205be9697498
Katharine <krsibbald@gmail.com>
2017-03-20 15:52:47 -0400

    You can sign up for a new account

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
